26850586a33ac2ec435c2a9133d7dae8878dc7d7
Oleg Gretsov <mrgretsov@yandex.ru>
2023-02-02 18:56:33 +0300

    added task_4

--- home_work_2/task_4.py
+++ home_work_2/task_4.py
@@ -3,3 +3,20 @@
 # *Пример:*
 #
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
+from random import randint
+k = int(input('Введите число '))
+def fibonacci(n):
+    a, b = 1, 1
+    for i in range(n):
+        yield a
+        a, b = b, a + b
+
+data = list(fibonacci(k))
+new_list = []
+for item in data:
+    new_list.append(-item)
+
+last_spisok = new_list + data
+last_spisok.append(0)
+last_spisok.sort()
+print(last_spisok)
