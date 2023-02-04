bd2d65add0e49a47cc1b871b38b5d93c6e3438e4
Oleg Gretsov <mrgretsov@yandex.ru>
2023-02-04 13:50:40 +0300

    update_task_5

--- home_work_2/task_5.py
+++ home_work_2/task_5.py
@@ -1,3 +1,26 @@
 # Напишите программу, которая принимает на вход координаты двух точек
 # и находит расстояние между ними в N-мерном пространстве.
 # Сначала задается N с клавиатуры, потом задаются координаты точек.
+N = int(input('Введите N-мерное пространство: '))
+# formula = ((X1-Y1)**2 + (X2-Y2)**2)**0.5
+coordinates = []
+coordinates_2 = []
+print()
+while True:
+    x = input("Координата числа X: ")
+    y = input("Координата числа Y: ")
+    coordinates_2.append(y)
+    coordinates.append(x)
+    N -= 1
+    if N == 0:
+        break
+print()
+print('Координаты первого числа: ',coordinates)
+print()
+print('Координаты второго числа: ',coordinates_2)
+n = 0
+rezult = 0
+while (n < len(coordinates)):
+    rezult += (int(coordinates[n]) - int(coordinates_2[n]))**2
+    n = n + 1
+print(rezult**0.5)
